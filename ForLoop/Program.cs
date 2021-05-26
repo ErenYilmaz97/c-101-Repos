684b67a60cddcd6b74a565e4386546f84eadfeeb
Eren YILMAZ <erenyilmazgazi@gmail.com>
2021-05-26 20:25:55 +0300

    for loop

---
+++ ForLoop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    break;
+                Console.WriteLine(i);
+            }
+
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    continue;
+                Console.WriteLine(i);
+            }
+
+
+            for (;;)
+            {
+                if (i == 4)
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
