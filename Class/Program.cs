c6ca6f27b32a962616888ca0926cf55779705b76
Eren YILMAZ <erenyilmazgazi@gmail.com>
2021-05-31 20:30:54 +0300

    class added

---
+++ Class/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad = "Eren";
+            calisan1.Soyad = "Yılmaz";
+            calisan1.No = 32131231;
+            calisan1.Departman = "Yazılım";
+
+
+            Calisan calisan2 = new Calisan();
+            calisan1.Ad = "Deniz";
+            calisan1.Soyad = "Arda";
+            calisan1.No = 32131231;
+            calisan1.Departman = "Pazarlama";
+
+            calisan1.CalisanBilgileri();
+            calisan2.CalisanBilgileri();
+        }
+    }
+
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine($"Çalışan Adı : {Ad}");
+            Console.WriteLine($"Çalışan Soyadı : {Soyad}");
+            Console.WriteLine($"Çalışan Numarası : {No}");
+            Console.WriteLine($"Çalışan Departmanı : {Departman}");
+        }
+    }
+}
